abace47f5f1f721e8948ada3f144bb924178a0ad
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2024-12-18 11:55:26 +0530

    create update listing api routes

--- api/routes/listening.route.js
+++ api/routes/listening.route.js
@@ -2,11 +2,14 @@ import express from 'express';
 import {
   createListening,
   deleteListing,
+  updateListing,
 } from "../controllers/Listening.controller.js";
 import { verifyToken } from '../utils/verifyUser.js';
 const router =express.Router();
 
 router.post('/create',verifyToken,createListening);
 router.delete('/delete/:id',verifyToken,deleteListing);
+
+router.post('/update/:id',verifyToken,updateListing);
 
 export default router;
